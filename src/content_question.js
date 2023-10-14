4d0e3828aa330eb0303fd4167ab105d0fdf006d5
高矢 空 <79049713+Tsora1216@users.noreply.github.com>
2023-10-14 14:11:15 +0900

    Change Speedup PNG

--- src/content_question.js
+++ src/content_question.js
@@ -1,4 +1,16 @@
 window.addEventListener('load', function () {
+    // PNGファイルを表示
+    var imageUrl = 'https://github.com/takayasora/super_musasi/raw/main/src/resource/speedup.png';
+    var imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    imgElement.style.position = 'fixed'; // 画像の位置を固定
+    imgElement.style.top = '515px'; // 画像の上からの位置を調整
+    imgElement.style.left = '270px'; // 画像の左からの位置を調整
+    imgElement.style.width = '400px'; // 画像の幅を調整
+    imgElement.style.height = '70px'; // 画像の高さを調整
+    imgElement.style.zIndex = '9999'; // Zインデックスを設定
+    document.body.appendChild(imgElement);
+
     var interval = setInterval(checkForSelectedSpan, 100);
     var buttonClicked = false; // ボタンがすでにクリックされたかどうかを示すフラグ
 
@@ -11,7 +23,9 @@ window.addEventListener('load', function () {
         if (selectedSpan) {
             // 監視を停止
             buttonA = document.getElementById('btn_answersAndGuidance')
+            buttonA = document.getElementById('btn_answersAndGuidance')
             buttonB = document.getElementById('btn_nextQuestion')
+            
             if (selectedSpan) {
                 if (buttonA){
                     document.getElementById('btn_answersAndGuidance').click();
